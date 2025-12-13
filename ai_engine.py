67bec9e89ff13ab619ca887f9add93d119ec4f5e
SaiHarshiniRakudhiti <saiharshini081105@gmail.com>
2025-12-13 20:39:59 +0530

    Update and rename ai_chat.py to ai_engine.py

---
+++ ai_engine.py
@@ -0,0 +1,12 @@
+ import openai
+
+openai.api_key = 'your-api-key'
+
+def ask_ai(prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response['choices'][0]['message']['content']
+
+
